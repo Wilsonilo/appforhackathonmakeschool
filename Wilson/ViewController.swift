b1c9c00293f4115cae4f5e5053ea9f0718418f58
Wilson Muñoz <contacto@maquilaweb.net>
2016-04-29 08:25:31 -0500

    Initial Commit

---
+++ Wilson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wilson
+//
+//  Created by Wilson Muñoz on 4/29/16.
+//  Copyright © 2016 Wilson Muñoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
